4be2860a3cab88442bbbb1d76c524a5a81518287
Shashwat Adhau <shashwatadhau3@gmail.com>
2023-06-27 13:04:54 +0530

    added remove checkd functionalyity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 12, packed: true },
-];
-
 const App = () => {
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
+  const handleRemoveItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  const handlePackedItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form addItem={addItem} />
+      <PackingList
+        onRemoveItem={handleRemoveItem}
+        onPackedItem={handlePackedItem}
+        items={items}
+      />
       <Stats />
     </div>
   );
@@ -21,24 +37,36 @@ const Logo = () => {
   return <h1>💕 Far Away 👌</h1>;
 };
 
-const Form = () => {
+const Form = (props) => {
   const [description, setDescription] = useState("");
   const [itemNumber, setItemNumber] = useState(1);
+  //const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    props.addItem(item);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(description, itemNumber);
+    if (!description) return;
+    const newItem = {
+      description,
+      quantity: itemNumber,
+      packed: false,
+      id: Date.now(),
+    };
+
+    handleAddItems(newItem);
+
     setDescription("");
     setItemNumber(1);
   };
 
   const handleInput = (e) => {
-    console.log(e.target.value);
     setDescription(e.target.value);
   };
 
   const handleSelect = (e) => {
-    console.log(e.target.value);
     setItemNumber(Number(e.target.value));
   };
 
@@ -63,12 +91,17 @@ const Form = () => {
   );
 };
 
-const PackingList = () => {
+const PackingList = (props) => {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {props.items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onPackedItem={props.onPackedItem}
+            onRemoveItem={props.onRemoveItem}
+          />
         ))}
       </ul>
     </div>
@@ -83,13 +116,18 @@ const Stats = () => {
   );
 };
 
-const Item = ({ item }) => {
+const Item = ({ item, onRemoveItem, onPackedItem }) => {
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={item.packed}
+        onChange={() => onPackedItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onRemoveItem(item.id)}>❌</button>
     </li>
   );
 };
